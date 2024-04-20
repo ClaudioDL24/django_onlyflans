79a14d4c9a92fa2bbf0d6e6e2a338c0f1758cf68
JPGarrido <jpgarridoinformatica@gmail.com>
2024-04-20 11:11:39 -0400

    Creacion Contactform

--- onlyflans_dj/static_pages/models.py
+++ onlyflans_dj/static_pages/models.py
@@ -1,3 +1,5 @@
+from email.policy import default
+from unittest.util import _MAX_LENGTH
 import uuid
 from django.db import models
 
@@ -9,3 +11,10 @@ class Flan(models.Model):
     image_url= models.URLField()
     slug= models.SlugField()
     is_private= models.BooleanField()
+
+class ContactForm(models.Model):
+    contact_form_uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    customer_email = models.EmailField()
+    customer_name = models.CharField(max_length=64)
+    message = models.TextField()
+
